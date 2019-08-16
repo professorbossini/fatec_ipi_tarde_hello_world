bae0bd2c434ff23aa7b94c021fc31a43be1bce67
rodrigo <professorbossini@gmail.com>
2019-08-16 15:57:18 -0300

    Initial commit. Terminamos o pdf.

---
+++ app/src/main/java/br/com/bossini/fatec_ipi_tarde_hello_world/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.bossini.fatec_ipi_tarde_hello_world;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
